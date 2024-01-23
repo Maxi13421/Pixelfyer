6374d42127dd00092fb2a1ab897c774f8ed4639d
Maxi13421 <magleiser@gmx.de>
2024-01-23 16:01:05 +0100

    .

--- concatenate.py
+++ concatenate.py
@@ -5,10 +5,10 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    image = Image.new("RGBA",(32*64,32*16))
+    image = Image.new("RGBA",(34*64,34*16))
     for tile in range(len(os.listdir("output"))):
         tilename = os.listdir("output")[tile]
         for edge in range(len(os.listdir("output/" + tilename))):
             edgename = os.listdir("output/" + tilename)[edge]
-            image.paste(Image.open("output/" + str(tile//8) + "_" + str(tile%8) + "/" + str(edge) + ".png"),(tile*32, edge*32))
+            image.paste(Image.open("output/" + str(tile//8) + "_" + str(tile%8) + "/" + str(edge) + ".png"),(tile*34, edge*34))
     image.save("tileset.png","PNG")
